7dc04b0d01721aef7285c9df6c69e0c95701292c
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-14 17:42:17 +0300

    fix Task 02 in lesson04 promise

--- src/lessons/lesson4/Lesson4.tsx
+++ src/lessons/lesson4/Lesson4.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './lesson_4';
 
+const createPromiseHandler = () => {
+  const promise = new Promise(() => {})
+  console.log(promise)
+}
 const Lesson4 = () => {
     return (
         <div>
-            <button id='btn-create-promise'>Create Promise</button>
+            <button onClick={createPromiseHandler} id='btn-create-promise'>Create Promise</button>
             <button id='btn-resolve-promise'>Resolve Promise</button>
             <button id='btn-reject-promise'>Reject Promise</button>
         </div>
--- src/lessons/lesson4/lesson_4.ts
+++ src/lessons/lesson4/lesson_4.ts
@@ -1,5 +1,6 @@
 
 
+
 console.log('lesson 4');
 
 // http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
@@ -9,18 +10,17 @@ console.log('lesson 4');
 // Task 01
 // Создайте промис, который постоянно находиться в состоянии pending.
 // В конструкторе промиса выведите в консоль сообщение "Promise is created".
-// const promise = new Promise(() => {
-//   console.log("Promise is created")
-// })
+const promise = new Promise(() => {
+  console.log("Promise is created")
+})
 // console.log(promise)
 // Task 02
 // Создайте промис, который после создания сразу же переходит в состояние resolve
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
-// const promiseTwo = new Promise((res, rej) => {
-//   res('Promise Data' )
-// })
-// console.log(promiseTwo)
+// const promiseTwo = new Promise((resolve) => {
+//   resolve("Promise Data");
+// }).then((data) => console.log(data));
 // Task 03
 // Создайте промис, который после создания сразу же переходит в состояние rejected
 // и возвращает строку 'Promise Error'
@@ -34,12 +34,12 @@ console.log('lesson 4');
 // (Используйте setTimeout)
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
-const promise = new Promise((res, rej) => {
-  setTimeout(() => {
-    res('Promise Data')
-  },3000)
-}).then(data => console.log(data))
-console.log(promise)
+// const promise = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     res('Promise Data')
+//   },3000)
+// }).then(data => console.log(data))
+// console.log(promise)
 // Task 05
 // Создайте литерал объекта handlePromise со следующими свойствами:
 // promise, resolve, reject, onSuccess, onError
@@ -52,7 +52,17 @@ console.log(promise)
 // описаного выше объекта: свойство promise получает новый созданный промис,
 // свойства resolve и reject получают ссылки на соответствующие функции
 // resolve и reject. Следующие два обработчика запускают методы resolve и reject.
-
+// export const handlePromise = {
+//   promise: null,
+//   resolve: null,
+//   reject: null,
+//   onSuccess: (paramName: string)=>{
+//     return `Promise is resolved with data: ${paramName}`
+//   },
+//   onError: (paramName: string) => {
+//     return `Promise is rejected with error: ${paramName}`
+//   }
+// }
 
 // Task 06
 // Создайте промис, который через 1 с возвращает строку "My name is".
@@ -60,7 +70,17 @@ console.log(promise)
 // прибавляет к нему Ваше имя и возвращает новую строку из функции
 // Создайте функцию print, которая выводит в консоль значение своего параметра
 // Добавьте два метода then и передайте созданные функции.
-
+const onSuccess = (param: string) => {
+  return param + ' Alex'
+}
+const print = (param: string) => {
+  console.log(param)
+}
+new Promise(() => {
+  setTimeout(() => {
+    return "My name is"
+  },1000)
+})
 
 // Task 7
 // Создайте три промиса. Первый промис возвращает объект { name: "Anna" } через 2с,
